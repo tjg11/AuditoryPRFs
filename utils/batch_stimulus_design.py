502b26543e6f245dd4d0533552d2cfe604264441
Taylor Garrison <tjg11@uw.edu>
2023-12-06 11:32:58 -0800

    Create batch_stimulus_design.py
    create file for batch running stimulus design

---
+++ utils/batch_stimulus_design.py
@@ -0,0 +1,10 @@
+import stimulus_design
+from dotenv import load_dotenv
+
+
+def batch_stimulus_design():
+    """Takes no parameters. Utilizes paths stored in .env to create stimulus
+    files for each subject listed in SUBJECT_IDS. Returns number of files
+    created."""
+    file_count = 0
+    return file_count
